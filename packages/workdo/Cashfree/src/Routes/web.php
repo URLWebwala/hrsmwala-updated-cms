2f087184d4d95ef945494e9996c8edb3b1bd2ee6
guddugiri1 <guddugiri216@gmail.com>
2026-04-10 10:42:17 +0530

    Fix: Imported CashfreeController in routes to fix BindingResolutionException

--- packages/workdo/Cashfree/src/Routes/web.php
+++ packages/workdo/Cashfree/src/Routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Workdo\Cashfree\Http\Controllers\CashfreeSettingsController;
+use Workdo\Cashfree\Http\Controllers\CashfreeController;
 
 Route::group(['middleware' => ['web', 'auth']], function () {
     Route::post('cashfree/settings/update', [CashfreeSettingsController::class, 'update'])->name('cashfree.settings.update');
